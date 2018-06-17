cd91dc2ba6aa12188106917feac9280575b10722
Jae-Seong Yang <jyang@ant-login6.linux.crg.es>
2018-06-17 11:05:57 +0200

    .

--- example/create.sh
+++ example/create.sh
@@ -1,5 +1,15 @@
-zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
-zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
-zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
-zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
-zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
+#zcat ../../2017-03-03_MiSeq/S1_R1.fastq.gz | head -n 100000 > ./fastq/EXP2_W_R1.25000.fastq
+#zcat ../../2017-03-03_MiSeq/S1_R2.fastq.gz | head -n 100000 > ./fastq/EXP2_W_R2.25000.fastq
+#zcat ../../2017-03-03_MiSeq/S3_R1.fastq.gz | head -n 100000 > ./fastq/EXP2_Q_R1.25000.fastq
+#zcat ../../2017-03-03_MiSeq/S3_R2.fastq.gz | head -n 100000 > ./fastq/EXP2_Q_R2.25000.fastq
+
+zcat ../../2017-08-22_MiSeq/S64-SWD_S1_R1.fastq.gz | head -n 100000 > ./fastq/EXP3_W_R1.25000.fastq
+zcat ../../2017-08-22_MiSeq/S64-SWD_S1_R2.fastq.gz | head -n 100000 > ./fastq/EXP3_W_R2.25000.fastq
+zcat ../../2017-08-22_MiSeq/S64-SQD_S4_R1.fastq.gz | head -n 100000 > ./fastq/EXP3_Q_R1.25000.fastq
+zcat ../../2017-08-22_MiSeq/S64-SQD_S4_R2.fastq.gz | head -n 100000 > ./fastq/EXP3_Q_R2.25000.fastq
+
+#zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
+#zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
+#zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
+#zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
+#zcat ../../2017-11-03_MiSeq/S1_W_R1.fastq.gz  | head -n 400000 > EXP1_W_R1.fastq
